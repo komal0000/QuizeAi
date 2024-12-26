c8be72057ab3442c0ea3820e70b212ff242ea46a
komal shrestha <komalstha0000.com>
2024-12-26 10:49:21 +0545

    migration changed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('username');
+            $table->text('username')->unique();
             $table->integer('age');
             $table->text('password');
             $table->rememberToken();
